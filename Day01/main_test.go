7de7282d17a204635385047adffdc5995d44ac1f
Jake Heidman <jake.heidman2@gmail.com>
2023-12-03 20:48:19 -0500

    function for extracting number from line of text created and passing tests

--- Day01/main_test.go
+++ Day01/main_test.go
@@ -50,58 +50,78 @@ func TestPart2(t *testing.T) {
 	}
 }
 
-func TestGetUpdatedCalibrationValue1(t *testing.T) {
-	got := GetUpdatedCalibrationValue("two1nine")
-	want := 29
-	if got != want {
+// func TestGetUpdatedCalibrationValue1(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("two1nine")
+// 	want := 29
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+// func TestGetUpdatedCalibrationValue2(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("eightwothree")
+// 	want := 83
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+// func TestGetUpdatedCalibrationValue3(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("abcone2threexyz")
+// 	want := 13
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+// func TestGetUpdatedCalibrationValue4(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("xtwone3four")
+// 	want := 24
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+// func TestGetUpdatedCalibrationValue5(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("4nineeightseven2")
+// 	want := 42
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+// func TestGetUpdatedCalibrationValue6(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("zoneight234")
+// 	want := 14
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+// func TestGetUpdatedCalibrationValue7(t *testing.T) {
+// 	got := GetUpdatedCalibrationValue("7pqrstsixteen")
+// 	want := 76
+// 	if got != want {
+// 		t.Errorf("got %d want %d", got, want)
+// 	}
+// }
+
+func TestGetWordNumber1(t *testing.T) {
+	got, err := getWordNumber("two1nine")
+	want := 2
+	if err != nil {
+		t.Errorf(err.Error())
+	} else if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
 
-func TestGetUpdatedCalibrationValue2(t *testing.T) {
-	got := GetUpdatedCalibrationValue("eightwothree")
-	want := 83
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-
-func TestGetUpdatedCalibrationValue3(t *testing.T) {
-	got := GetUpdatedCalibrationValue("abcone2threexyz")
-	want := 13
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-
-func TestGetUpdatedCalibrationValue4(t *testing.T) {
-	got := GetUpdatedCalibrationValue("xtwone3four")
-	want := 24
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-
-func TestGetUpdatedCalibrationValue5(t *testing.T) {
-	got := GetUpdatedCalibrationValue("4nineeightseven2")
-	want := 42
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-
-func TestGetUpdatedCalibrationValue6(t *testing.T) {
-	got := GetUpdatedCalibrationValue("zoneight234")
-	want := 14
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
-}
-
-func TestGetUpdatedCalibrationValue7(t *testing.T) {
-	got := GetUpdatedCalibrationValue("7pqrstsixteen")
-	want := 76
-	if got != want {
+func TestGetWordNumber2(t *testing.T) {
+	got, err := getWordNumber("eightwothree")
+	want := 8
+	if err != nil {
+		t.Errorf(err.Error())
+	} else if got != want {
 		t.Errorf("got %d want %d", got, want)
 	}
 }
