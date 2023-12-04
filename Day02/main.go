4a8b60c349857f8bfbffdaffa8c5dc101c6a241c
Jake Heidman <jake.heidman2@gmail.com>
2023-12-03 23:07:44 -0500

    Input line to game struct function and passing test

--- Day02/main.go
+++ Day02/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+type game struct {
+	id    int
+	red   int
+	blue  int
+	green int
+}
+
+// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+func LineToGame(line string) *game {
+	splitOnGame := strings.Split(line, ": ") //Game 1
+	gameSubstring := splitOnGame[0]
+	ballsSubstring := splitOnGame[1]
+	ballsSubstring = strings.Replace(ballsSubstring, ";", ",", -1)
+	var red, green, blue int
+	gameID, _ := strconv.Atoi(strings.TrimPrefix(gameSubstring, "Game "))
+	ballDraws := strings.Split(ballsSubstring, ", ")
+	for _, draw := range ballDraws {
+		numBalls, _ := strconv.Atoi(string(draw[0]))
+		if strings.Contains(draw, "blue") {
+			blue = max(blue, numBalls)
+		} else if strings.Contains(draw, "red") {
+			red = max(red, numBalls)
+		} else if strings.Contains(draw, "green") {
+			green = max(green, numBalls)
+		} else {
+			log.Fatal("error when trying to decipher single draw: " + draw)
+		}
+	}
+	game := game{id: gameID,
+		red:   red,
+		blue:  blue,
+		green: green}
+	return &game
+}
+
 func Day2Part1(filename string) int {
+	// input := helpers.ParseInput(filename)
+	// for _, line := range input {
+
+	// }
 	return 0
+}
+
+func main() {
+	fmt.Println(Day2Part1("test_input1.txt"))
 }
--- Day02/main_test.go
+++ Day02/main_test.go
@@ -1,11 +1,38 @@
 package main
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
 
 func TestPart1(t *testing.T) {
 	got := Day2Part1("test_input1.txt")
 	want := 8
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func compareGames(g1 *game, g2 *game) (string, bool) {
+	if g1.id != g2.id {
+		return "id", false
+	}
+	if g1.red != g2.red {
+		return ("red: " + strconv.Itoa(g1.red) + " vs " + strconv.Itoa(g2.red)), false
+	}
+	if g1.green != g2.green {
+		return "green", false
+	}
+	if g1.blue != g2.blue {
+		return "blue", false
+	}
+	return "", true
+}
+
+func TestLineToGame(t *testing.T) {
+	got := LineToGame("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+	want := &game{id: 1, red: 4, green: 2, blue: 6}
+	if err, state := compareGames(got, want); !state {
+		t.Errorf("field did not match: " + err)
 	}
 }
