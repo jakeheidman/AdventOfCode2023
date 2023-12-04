726733aaa3765471ccc41ecc3c6f094602a993ff
Jake Heidman <jake.heidman2@gmail.com>
2023-12-03 19:08:17 -0500

    part 1 tests and solution

--- Day01/main.go
+++ Day01/main.go
@@ -1,11 +1,59 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
+func ParseInput(filename string) []string {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return lines
+}
+
+func isInt(char rune) bool {
+	return char >= '0' && char <= '9'
+}
+
+func GetCalibrationValue(line string) int {
+	var first_digit, last_digit rune
+	for _, char := range line {
+		if isInt(char) {
+			if first_digit == 0 {
+				first_digit = char
+			}
+			last_digit = char
+
+		}
+	}
+	number, _ := strconv.Atoi(string(first_digit) + string(last_digit))
+	return number
+}
+
 func Part1(filename string) int {
-	return 0
+	input := ParseInput(filename)
+	var sum int
+	for _, line := range input {
+		calibrationValue := GetCalibrationValue(line)
+		sum += calibrationValue
+	}
+	return sum
 }
 
 func Part2() int {
--- Day01/main_test.go
+++ Day01/main_test.go
@@ -3,9 +3,41 @@ package main
 import "testing"
 
 func TestDay01(t *testing.T) {
-	got := Part1("test_input.txt")
+	got := Part1("test_input1.txt")
 	want := 142
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestGetCalibrationValue1(t *testing.T) {
+	got := GetCalibrationValue("1abc2")
+	want := 12
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestGetCalibrationValue2(t *testing.T) {
+	got := GetCalibrationValue("pqr3stu8vwx")
+	want := 38
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestGetCalibrationValue3(t *testing.T) {
+	got := GetCalibrationValue("a1b2c3d4e5f")
+	want := 15
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
+
+func TestGetCalibrationValue4(t *testing.T) {
+	got := GetCalibrationValue("treb7uchet")
+	want := 77
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
 	}
 }
