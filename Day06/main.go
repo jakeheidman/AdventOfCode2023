2c8c12388b9b6e19596c18ea8fcc157fd9b0839f
Jake Heidman <jake.heidman2@gmail.com>
2023-12-14 17:31:52 -0500

    working part 1 solution

--- Day06/main.go
+++ Day06/main.go
@@ -2,13 +2,73 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/jakeheidman/AdventOfCode2023/helpers"
 )
 
+type race struct {
+	time   int
+	record int
+}
+
+func makeRace(time, record int) *race {
+	r := new(race)
+	r.time = time
+	r.record = record
+	return r
+}
+
+func (r *race) numRecordPermutations() int {
+	permutations := 0
+	for t := 1; t < r.time; t++ {
+		remainingTime := r.time - t
+		distance := remainingTime * t
+		if distance > r.record {
+			permutations++
+		}
+	}
+	return permutations
+}
+
 func Part1(filename string) int {
 	input := helpers.ParseInput(filename)
-	return 0
+	times := input[0]
+	distances := input[1]
+	races := getRaces(times, distances)
+	answer := 1
+	for _, r := range races {
+		p := r.numRecordPermutations()
+		answer *= p
+	}
+	return answer
+}
+
+func getRaces(times, distances string) []*race {
+	var races []*race
+	var allTimes []int
+	var allDistances []int
+	times = strings.TrimLeft(times, "Time: ")
+	distances = strings.TrimLeft(distances, "Distance: ")
+	for _, time := range strings.Split(times, " ") {
+		if time != "" {
+			t, _ := strconv.Atoi(time)
+			allTimes = append(allTimes, t)
+		}
+	}
+	for _, dist := range strings.Split(distances, " ") {
+		if dist != "" {
+			d, _ := strconv.Atoi(dist)
+			allDistances = append(allDistances, d)
+		}
+	}
+	for i := 0; i < len(allTimes); i++ {
+		newRace := makeRace(allTimes[i], allDistances[i])
+		races = append(races, newRace)
+	}
+
+	return races
 }
 
 func Part2(filename string) int {
@@ -16,5 +76,6 @@ func Part2(filename string) int {
 }
 
 func main() {
-	fmt.Println(Part1("test_input.txt"))
+	//getRaces("Time:      7  15   30", "")
+	fmt.Println(Part1("input.txt"))
 }
--- Day06/main_test.go
+++ Day06/main_test.go
@@ -7,3 +7,12 @@ import (
 func TestPart1(t *testing.T) {
 
 }
+
+func TestNumRecordPermutations(t *testing.T) {
+	r := makeRace(7, 9)
+	got := r.numRecordPermutations()
+	want := 4
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+}
