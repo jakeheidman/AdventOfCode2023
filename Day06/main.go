9d655c58f5659044b47ff5f57fc9f1876e18f70c
Jake Heidman <jake.heidman2@gmail.com>
2023-12-14 17:09:51 -0500

    day 6 skeleton

---
+++ Day06/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jakeheidman/AdventOfCode2023/helpers"
+)
+
+func Part1(filename string) int {
+	input := helpers.ParseInput(filename)
+	return 0
+}
+
+func Part2(filename string) int {
+	return 0
+}
+
+func main() {
+	fmt.Println(Part1("test_input.txt"))
+}
